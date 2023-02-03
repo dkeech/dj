3278c7f83828f76f14e8dde9decaec5898890fe7
Dan <ddkeech@gmail.com>
2023-02-02 21:50:15 -0500

    pages & routing

---
+++ pages/about.js
@@ -0,0 +1,8 @@
+export default function About() {
+    return (
+        <div>
+            <h1>About</h1>
+            <p className="">This is a page to view details about events</p>
+        </div>
+    )
+}
---
+++ pages/events/add.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function add() {
+  return (
+    <div><h1>Add Event page</h1></div>
+  )
+}
